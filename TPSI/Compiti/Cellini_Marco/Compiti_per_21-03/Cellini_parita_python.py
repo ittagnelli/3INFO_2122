5159572b36c0d94a268747942a14e60a86db083d
Marco Cellini <marco.cellini@istitutoangnelli.it>
2022-03-19 15:27:30 +0100

    Compiti Cellini TPSI

--- TPSI/Compiti/Cellini_Marco/Compiti_per_21-03/Cellini_parita_python.py
+++ TPSI/Compiti/Cellini_Marco/Compiti_per_21-03/Cellini_parita_python.py
@@ -1,9 +1,3 @@
-def parita(l, codice, tot):
-    if tot % 2 == 0:
-        stampa(l, codice, 0)
-    else:
-        stampa(l, codice, 1)
-
 def stampa(l, codice, x):
     print ("\n\nIl bit di parita' pari e'", int(x), "e il codice finale e' --> ", end="")
     for i in range(l):
@@ -13,7 +7,14 @@ def stampa(l, codice, x):
     print ("\n\nIl bit di parita' dispari e'", int(not(x)), "e il codice finale e' --> ", end="")
     for i in range(l):
         print(codice[i], end="")
-    print (int(not (x)))    
+    print (int(not (x))) 
+    
+def parita(l, codice, tot):
+    if tot % 2 == 0:
+        stampa(l, codice, 0)
+    else:
+        stampa(l, codice, 1)
+
 
 l = input("\n\nInserisci la lunghezza del codice:\t")           #inserire un dato dall'utente
 l = int (l)
@@ -28,5 +29,4 @@ for i in range(l):
     codice.append(a)                # lo inserisco nell'array
     tot += a                          #faccio la somma totale
 
-if tot % 2 == 0:
-    parita(l, codice, tot)
+parita(l, codice, tot)
