e5f13cb0418eb7dcde69b6eef2fc2b64d701d1cd
Marco-Tranaso <79847085+Marco-Tranaso@users.noreply.github.com>
2022-03-21 17:25:54 +0100

    Update tranaso_parity.c

--- TPSI/Compiti/Tranaso_Marco/tranaso_parity.c
+++ TPSI/Compiti/Tranaso_Marco/tranaso_parity.c
@@ -28,8 +28,7 @@ void main(){
         s=s+n;
     }
     if (s%2!=0)
-
-    {
+{
         parit=1;
         disparit=0;
 
@@ -41,18 +40,14 @@ void main(){
         printf("%d", parit);
         }
         else
-        {
         printf("parametro non valido");
-        }
         if(parola=='D')
         {
         printf("Il bit di parita dispari per ");
         print(numero,l);
         printf(" e ");
         printf("%d", disparit);
-        
-        
+        }
+   }
+}
 
-    }
-}
-}
