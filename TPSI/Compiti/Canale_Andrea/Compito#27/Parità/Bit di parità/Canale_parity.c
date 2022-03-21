34ef157dd7ba8d8d29208436386570f677595276
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-03-21 15:00:53 +0100

    add compito#27 and cleanup

---
+++ TPSI/Compiti/Canale_Andrea/Compito#27/Parità/Bit di parità/Canale_parity.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int conto(int sim[], int dim) {
+	int count = 0;
+	for (int i = 0; i < dim; i++) {
+		if (sim[i] == 1) {
+			count++;
+		}
+	}
+	printf("\n");
+	return count;
+}
+void controlloparit(int conto, char tipo, char* numeri) {
+	if (tipo == 'P') {
+		if (conto % 2 == 0) {
+			printf("Il bit di parità per la parola %s %c 0\n", numeri,138);
+
+		}
+		else {
+			printf("Il bit di parità per la parola %s %c 1\n", numeri,138);
+
+		}
+	}
+	else if (tipo == 'D') {
+		if (conto % 2 == 0) {
+			printf("Il bit di parità per la parola %s %c 1\n", numeri, 138);
+		}
+		else {
+			printf("Il bit di parità per la parola %s %c 0\n", numeri,138);
+		}
+	}
+	else {
+		printf("Valore invalido, specificare P o D");
+	}
+
+}
+void main(int argc, char* argv[])
+{
+	char tipo;
+	int counter;
+	if (argc == 1) {
+		printf("Usage: ./Compito#27 P|D <parola binaria>\n");
+	}
+	else {
+		int binari[sizeof(argv[2])];
+		for (int i = 0; i < sizeof(argv[2]) - 1; i++)
+		{
+			binari[i] = argv[2][i] - '0';
+		}
+		controlloparit(conto(binari, sizeof(binari)), argv[1][0], argv[2]);
+	}
+
+}
